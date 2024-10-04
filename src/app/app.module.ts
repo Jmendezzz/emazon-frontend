8d0bb824a56dfff93a23f7e6b166d5c287dd8d4b
Juan Gerardo <jmendez1025@cue.edu.co>
2024-10-03 23:46:44 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/atoms/button/button.component';
+import { LinkComponent } from './components/atoms/link/link.component';
+import { LogoComponent } from './components/atoms/logo/logo.component';
+import { NavbarComponent } from './components/molecule/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    LinkComponent,
+    LogoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
