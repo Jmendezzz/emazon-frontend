0c002ec7ee384bcc58f36280273273223f187422
Juan Gerardo <jmendez1025@cue.edu.co>
2024-11-10 15:51:49 -0500

    feat: not found page implemented

--- src/app/components/atoms/atoms.module.ts
+++ src/app/components/atoms/atoms.module.ts
@@ -14,6 +14,7 @@ import {
 import { RouterModule } from '@angular/router';
 import { InputDateComponent } from './input-date/input-date.component';
 import { LogoutButtonComponent } from './logout-button/logout-button.component';
+import { NotFoundPageComponent } from '../pages/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { LogoutButtonComponent } from './logout-button/logout-button.component';
     LoaderComponent,
     InputDateComponent,
     LogoutButtonComponent,
+    NotFoundPageComponent
   ],
   imports: [CommonModule, RouterModule],
   exports: [
--- src/app/components/pages/not-found-page/not-found-page.component.spec.ts
+++ src/app/components/pages/not-found-page/not-found-page.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NotFoundPageComponent } from './not-found-page.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('NotFoundPageComponent', () => {
   let component: NotFoundPageComponent;
@@ -8,7 +9,8 @@ describe('NotFoundPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NotFoundPageComponent ]
+      declarations: [ NotFoundPageComponent ],
+      schemas: [NO_ERRORS_SCHEMA],
     })
     .compileComponents();
 
